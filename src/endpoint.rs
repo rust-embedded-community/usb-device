26732972803634989dc8c20e6e67df0b2c7d031e
Julien Cretin <cretin@google.com>
2023-04-12 15:29:50 +0200

    Support architectures without atomics with polyfill

--- src/endpoint.rs
+++ src/endpoint.rs
@@ -1,7 +1,7 @@
 use crate::bus::UsbBus;
 use crate::{Result, UsbDirection};
 use core::marker::PhantomData;
-use core::sync::atomic::{AtomicPtr, Ordering};
+use atomic_polyfill::{AtomicPtr, Ordering};
 
 /// Trait for endpoint type markers.
 pub trait EndpointDirection {
