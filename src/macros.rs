495c09afe1f2b8d8960af5c5c9bf568cb5868839
Ryan Summers <ryan.summers@vertigo-designs.com>
2024-02-26 13:51:01 +0100

    Adding logging macros

---
+++ src/macros.rs
@@ -0,0 +1,24 @@
+#[cfg(feature = "log")]
+macro_rules! usb_log {
+    (trace, $($arg:expr),*) => { log::trace!($($arg),*) };
+    (debug, $($arg:expr),*) => { log::trace!($($arg),*) };
+}
+
+#[cfg(feature = "defmt")]
+macro_rules! net_log {
+    (trace, $($arg:expr),*) => { defmt::trace!($($arg),*) };
+    (debug, $($arg:expr),*) => { defmt::debug!($($arg),*) };
+}
+
+#[cfg(not(any(feature = "log", feature = "defmt")))]
+macro_rules! usb_log {
+    ($level:ident, $($arg:expr),*) => {{ $( let _ = $arg; )* }}
+}
+
+macro_rules! usb_trace {
+    ($($arg:expr),*) => (usb_log!(trace, $($arg),*));
+}
+
+macro_rules! usb_debug {
+    ($($arg:expr),*) => (usb_log!(debug, $($arg),*));
+}
